d927c458f586784ef1d43fac17000923c46486dd
Rhattab0120 <randyhattab.one@gmail.com>
2021-07-26 19:16:03 -0400

    Initial Start

---
+++ game.js
@@ -0,0 +1,17 @@
+kaboom({
+  global: true,
+  fullscreen: true,
+  scale: 1,
+  debug: true,
+  clearColor: [0, 0, 0, 1]
+})
+
+loadSprite();
+
+scene("game", () => {
+  layers(["bg", "obj", "ui"], 'obj')
+
+
+})
+
+start("game")
