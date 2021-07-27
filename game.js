26c6f4007a518518bd2dbf8785e8a4488b8fe0ad
Rhattab0120 <randyhattab.one@gmail.com>
2021-07-26 20:05:49 -0400

    headbump

--- game.js
+++ game.js
@@ -69,13 +69,51 @@ scene("game", () => {
 
   add([text('level ' + 'test', pos(4, 6))])
 
+  function big() {
+    let timer = 0
+    let isBig = false
+
+    return {
+      update() {
+        if (isBig) {
+          timer -= dt()
+
+          if (timer <= 0) {
+            this.smallify()
+          }
+        }
+      },
+      isBig() {
+        return isBig
+      },
+      smallify() {
+        this.scale = vec2(1)
+        timer = 0
+        isBig = false
+      },
+      biggify(time) {
+        this.scale = vec2(2)
+        timer = time
+        isBig = false
+      }
+    }
+  }
+
   const player = add([
     sprite('mario'), 
     solid(), 
     pos(30, 0), 
     body(), 
+    big(),
     origin('bot')
   ])
+
+  player.on("headbump", (obj) => {
+    if (obj.is('coin-surprise')) {
+      gameLevel.spawn('$', obj.gridPos.sub(0,1))
+      destroy(obj)
+    }
+  })
 
   keyDown('left', () => {
     player.move(-MOVE_SPEED, 0)
